827dfe018c835d052eda59f48b4fce5443cd1364
Atharvatathe <atharva.tathe55@gmail.com>
2021-05-20 12:45:17 +0530

    Added: Calculate the employee wage for Month

--- employeeWage.js
+++ employeeWage.js
@@ -1,27 +1,29 @@
-  //UC3 Write Function for Daily Working Hours
+//UC4: Calculate Wage for Month
 
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
-const WAGE_PER_HOUR = 20;
-
-//Function to calculate Daily working Hours
-function getWorkingHours(empCheck){
-switch(empCheck){
-	case IS_PART_TIME:
-		return PART_TIME_HOURS;
-	
-	  case IS_FULL_TIME:
-		  return FULL_TIME_HOURS;
-		  
-	  default:
-		  return 0;
-	}
-}
-
+const WAGE_PER_HOUR = 20;   
+const NUM_OF_WORKING_DAYS = 2;
 let empHrs = 0;
-empCheck = Math.floor(Math.random() * 10) % 3;
-empHrs = getWorkingHours(empCheck);
+
+for(let day = 0; day < NUM_OF_WORKING_DAYS; day++){
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    empHrs += getWorkingHours(empCheck);
+	}
 let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Emp Wage Using function: " +empWage);
+console.log("Total Hrs: " +empHrs+ " Emp Wage: " + empWage);
+
+function getWorkingHours(empCheck){
+    switch(empCheck){
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        
+         case IS_FULL_TIME:
+             return FULL_TIME_HOURS;
+              
+         default:
+             return 0;
+		}
+}
