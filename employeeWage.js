6bde38e33c40deada98ff84f53c29523ac8113ce
Atharvatathe <atharva.tathe55@gmail.com>
2021-05-20 12:38:56 +0530

    Added: Calculate Daily Working Hours

--- employeeWage.js
+++ employeeWage.js
@@ -1,4 +1,4 @@
- //UC2: Calculte Daily Employee Wage
+  //UC3 Write Function for Daily Working Hours
 
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
@@ -6,19 +6,22 @@ const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 
+//Function to calculate Daily working Hours
+function getWorkingHours(empCheck){
+switch(empCheck){
+	case IS_PART_TIME:
+		return PART_TIME_HOURS;
+	
+	  case IS_FULL_TIME:
+		  return FULL_TIME_HOURS;
+		  
+	  default:
+		  return 0;
+	}
+}
+
 let empHrs = 0;
 empCheck = Math.floor(Math.random() * 10) % 3;
-switch(empCheck){
-  case IS_PART_TIME:
-	  empHrs = PART_TIME_HOURS;
-	  break;
-
-	case IS_FULL_TIME:
-		empHrs = FULL_TIME_HOURS;
-		break;
-		
-	default:
-		empHrs = 0;
-}
+empHrs = getWorkingHours(empCheck);
 let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Emp Wage using Switch case: " +empWage);  
+console.log("Emp Wage Using function: " +empWage);
