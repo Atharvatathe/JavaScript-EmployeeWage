cf39f74fdace464321ecf879f91f3ab83fcd2018
Atharvatathe <atharva.tathe55@gmail.com>
2021-05-20 12:58:24 +0530

    Added:calculate Employee wage till number of working Days using array

--- employeeWage.js
+++ employeeWage.js
@@ -1,23 +1,32 @@
-//UC5: Calculating the Wage till number of Working Days
-
+//UC6: calculate Employee wage till number of working Days using array
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
-const MAX_HRS_IN_MONTH = 100;
-const NUM_OF_WORKING_DAYS = 10;
+const MAX_HRS_IN_MONTH = 160;
+const NUM_OF_WORKING_DAYS = 20;
 let empHrs = 0;
 let totalEmpHrs = 0;
 let totalWorkingDays = 0;
-while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS ){
-    totalWorkingDays++;
-    let empCheck = Math.floor(Math.random() * 10) % 3;
-    totalEmpHrs += getWorkingHours(empCheck);
- 
-}
-let empWage = totalEmpHrs * WAGE_PER_HOUR;
-console.log("UC5 - Total Days: " +totalWorkingDays+ " Total hrs: " +totalEmpHrs+ " Emp Wage: " +empWage );
+
+function calcDailyWage(emphrs){
+        return empHrs * WAGE_PER_HOUR;
+    }
+
+let empDailyWageArr = new Array();
+
+while (totalEmpHrs <= MAX_HRS_IN_MONTH
+	&& totalWorkingDays < NUM_OF_WORKING_DAYS){
+		totalWorkingDays++;
+		let empCheck = Math.floor(Math.random() * 10) % 3;
+		let empHrs = getWorkingHours(empCheck);
+		totalEmpHrs= empHrs;
+		empDailyWageArr.push(calcDailyWage(empHrs));
+	}
+	let empWage = calcDailyWage(totalEmpHrs);
+	console.log("UC6 - Total Days: " + totalWorkingDays +
+				"Total Hrs: " + totalEmpHrs + " Emp Wage: " +empWage);
 
 function getWorkingHours(empCheck){
     switch(empCheck){
