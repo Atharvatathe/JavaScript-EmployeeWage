a7c10bb05251020b6ac770eab1c3f1d6047bda07
Atharvatathe <atharva.tathe55@gmail.com>
2021-05-21 11:36:46 +0530

    Added: Employee Wage with Arrow function

--- employeeWage.js
+++ employeeWage.js
@@ -1,4 +1,5 @@
-//UC8:Store Day and DailyWage along total wage using map 
+//UC9:Arrow Function
+
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
@@ -13,7 +14,7 @@ let totalWorkingDays = 0;
 
 let empDailyWageArr = new Array();
 let empDailyWageMap = new Map();
-
+let empDailyHrsMap = new Map();
 function calcDailyWage(empHrs){
         return empHrs * WAGE_PER_HOUR;
     }
@@ -26,6 +27,7 @@ while (totalEmpHrs <= MAX_HRS_IN_MONTH
 		totalEmpHrs += empHrs;
 		empDailyWageArr.push(calcDailyWage(empHrs));
 		empDailyWageMap.set(totalWorkingDays, calcDailyWage(empHrs));
+		empDailyHrsMap.set(totalWorkingDays,empHrs);
 	}
 
 function getWorkingHours(empCheck){
@@ -45,5 +47,28 @@ console.log(empDailyWageMap);
 function totalWages(totalWage, dailyWage){
 	return totalWage + dailyWage;
 }
-console.log("UC8 - Emp Wage Map totalHrs : " + Array.from(empDailyWageMap.values()).reduce(totalWages, 0));
+
+const findTotal = (totalval, dailyval) => {
+	return totalval + dailyval;
+}
+
+let count =0;
+let totalHours = Array.from(empDailyHrsMap.values()).filter(dailyHours => dailyHours > 0).reduce(findTotal,0);
+let totalSalary = empDailyWageArr.filter(dailyWage => dailyWage > 0).reduce(findTotal,0);
+
+console.log("Uc9 - Emp Wage with Arraow: " + " Total Hours: " + totalHours + " Total Wages: "+ totalSalary);
+
+
+let nonWorkingDays = new Array();
+let partWorkingDays = new Array();
+let fullWorkingDays = new Array();
+empDailyHrsMap.forEach((value,key,map) => {
+	if(value == 8) fullWorkingDays.push(key);
+	else if(value == 4) partWorkingDays.push(key);
+	else nonWorkingDays.push(key);
+});
+
+console.log("Full Working Days: " +fullWorkingDays);
+console.log("Part Working Days: " +partWorkingDays);
+console.log("Non Working Days: " +nonWorkingDays);
 
