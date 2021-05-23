f599356f2e76d3b1468029e06485f1c7038e60d4
Atharvatathe <atharva.tathe55@gmail.com>
2021-05-23 15:55:32 +0530

    Added: Extend Employee payroll Data Class using Gender and Date

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -2,25 +2,34 @@ class EmployeePayrollData{
     //property
     id;
     salary;
-    
+    gender;
+    startDate;
     //constructor
-    constructor(id,name,salary){
-        this.id = id;
-        this.salary = salary;
-        this.name = name;
+    constructor(...params){
+        this.id = params[0];
+        this.salary = params[1];
+        this.name = params[2];
+        this.gender = params[3];
+        this.startDate = params[4];
     }
 
     //getter and setter
-    get name(){return this.name;}
+    get name(){return this._name;}
     set name(name){this._name = name;}
 
     //method
     toString(){
-        return "id=" +this.id+ ", name=" +this.name+ ", salary=" +this.salary;
+        const options = {year: 'numeric', month: 'long', day: 'numeric'};   
+        const empDate = this.startDate === undefined ? "undefined" :
+                        this.startDate.toLocaleDateString("en-US",options);
+        return "id=" +this.id+ ", name=" +this.name+ ", salary=" +this.salary+
+        ", Gender=" +this.gender+ ", startDate= " +empDate ;
     }
 }
 
 let employeePayrollData = new EmployeePayrollData(1,"Mark",50000);
-console.log(employeePayrollData.toString);
+console.log(employeePayrollData.toString());
 employeePayrollData.name = "John";
-console.log(employeePayrollData.toString);
+console.log(employeePayrollData.toString());
+let newEmployeepayrollData = new EmployeePayrollData(1,"Terisa",30000,"F",new Date());
+console.log(newEmployeepayrollData.toString());
